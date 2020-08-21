4278e813ab635d93b17a5c2aefab483fdbb2f455
theainerd <beingprofess@gmail.com>
2020-08-21 23:16:49 +0530

    updated transformation functions

--- utils/tranform_functions.py
+++ utils/tranform_functions.py
@@ -768,105 +768,105 @@ def clinc_out_of_scope_to_tsv(dataDir, readFile, wrtDir, transParamDict, isTrain
     testF.close()
 
 
-    def kaggle_ner_pos_to_tsv(dataDir, readFile, wrtDir, transParamDict, isTrainFile=False):
+def kaggle_ner_pos_to_tsv(dataDir, readFile, wrtDir, transParamDict, isTrainFile=False):
 
-        """
-        This function transforms the data present in ner_pos_data/.
-        Raw data is in csv format format with the POS and NER tags separated by comma.
-        The transformation function converts the each raw data file into two separate tsv files,
-        one for POS tagging task and another for NER task. Following transformed files are written at wrtDir
+    """
+    This function transforms the data present in ner_pos_data/.
+    Raw data is in csv format format with the POS and NER tags separated by comma.
+    The transformation function converts the each raw data file into two separate tsv files,
+    one for POS tagging task and another for NER task. Following transformed files are written at wrtDir
 
-        - NER transformed tsv file.
-        - NER label map joblib file.
-        - POS transformed tsv file.
-        - POS label map joblib file.
+    - NER transformed tsv file.
+    - NER label map joblib file.
+    - POS transformed tsv file.
+    - POS label map joblib file.
 
-        For using this transform function, set ``transform_func`` : **kaggle_ner_to_tsv** in transform file.
+    For using this transform function, set ``transform_func`` : **kaggle_ner_to_tsv** in transform file.
 
-        Args:
-            dataDir (:obj:`str`) : Path to the directory where the raw data files to be read are present..
-            readFile (:obj:`str`) : This is the file which is currently being read and transformed by the function.
-            wrtDir (:obj:`str`) : Path to the directory where to save the transformed tsv files.
-            transParamDict (:obj:`dict`, defaults to :obj:`None`): Dictionary of function specific parameters. Not required for this transformation function.
+    Args:
+        dataDir (:obj:`str`) : Path to the directory where the raw data files to be read are present..
+        readFile (:obj:`str`) : This is the file which is currently being read and transformed by the function.
+        wrtDir (:obj:`str`) : Path to the directory where to save the transformed tsv files.
+        transParamDict (:obj:`dict`, defaults to :obj:`None`): Dictionary of function specific parameters. Not required for this transformation function.
 
-        """
+    """
 
 
-        csv_file = open(os.path.join(dataDir, readFile))
-        f = reader(csv_file)
+    csv_file = open(os.path.join(dataDir, readFile))
+    f = reader(csv_file)
 
-        # Skip the header part
+    # Skip the header part
 
-        header = next(f)
+    header = next(f)
 
-        nerW = open(os.path.join(wrtDir, 'ner_{}.tsv'.format(readFile.split('.')[0])), 'w')
-        posW = open(os.path.join(wrtDir, 'pos_{}.tsv'.format(readFile.split('.')[0])), 'w')
+    nerW = open(os.path.join(wrtDir, 'ner_{}.tsv'.format(readFile.split('.')[0])), 'w')
+    posW = open(os.path.join(wrtDir, 'pos_{}.tsv'.format(readFile.split('.')[0])), 'w')
 
-        labelMapNer = {}
-        labelMapPos = {}
+    labelMapNer = {}
+    labelMapPos = {}
 
-        sentence = []
-        senLens = []
-        labelNer = []
-        labelPos = []
-        uid = 0
+    sentence = []
+    senLens = []
+    labelNer = []
+    labelPos = []
+    uid = 0
 
-        print("Making data from file {} ...".format(readFile))
+    print("Making data from file {} ...".format(readFile))
 
-        for i, line in enumerate(f):
-            if i%5000 == 0:
-                print("Processing {} rows...".format(i))
+    for i, line in enumerate(f):
+        if i%5000 == 0:
+            print("Processing {} rows...".format(i))
 
-            wordSplit = line
+        wordSplit = line
 
-            if len(line[1])=='.' or line[0].startswith('Sentence'):
+        if len(line[1])=='.' or line[0].startswith('Sentence'):
 
-                if len(sentence) > 0:
+            if len(sentence) > 0:
 
-                    nerW.write("{}\t{}\t{}\n".format(uid, labelNer, sentence))
-                    posW.write("{}\t{}\t{}\n".format(uid, labelPos, sentence))
-                    senLens.append(len(sentence))
+                nerW.write("{}\t{}\t{}\n".format(uid, labelNer, sentence))
+                posW.write("{}\t{}\t{}\n".format(uid, labelPos, sentence))
+                senLens.append(len(sentence))
 
-                    #print("len of sentence :", len(sentence))
+                #print("len of sentence :", len(sentence))
 
-                    sentence = []
-                    labelNer = []
-                    labelPos = []
-                    uid += 1
-                continue
+                sentence = []
+                labelNer = []
+                labelPos = []
+                uid += 1
+            continue
 
-            sentence.append(wordSplit[1])
-            labelPos.append(wordSplit[-2])
-            labelNer.append(wordSplit[-1])
+        sentence.append(wordSplit[1])
+        labelPos.append(wordSplit[-2])
+        labelNer.append(wordSplit[-1])
 
-            if isTrainFile:
-                if wordSplit[-1] not in labelMapNer:
-                    # ONLY TRAIN FILE SHOULD BE USED TO CREATE LABEL MAP FILE.
-                    labelMapNer[wordSplit[-1]] = len(labelMapNer)
-                if wordSplit[-2] not in labelMapPos:
-                    labelMapPos[wordSplit[-2]] = len(labelMapPos)
+        if isTrainFile:
+            if wordSplit[-1] not in labelMapNer:
+                # ONLY TRAIN FILE SHOULD BE USED TO CREATE LABEL MAP FILE.
+                labelMapNer[wordSplit[-1]] = len(labelMapNer)
+            if wordSplit[-2] not in labelMapPos:
+                labelMapPos[wordSplit[-2]] = len(labelMapPos)
 
-        print("NER File Written at {}".format(wrtDir))
-        print("POS File Written at {}".format(wrtDir))
-        #writing label map
-        if labelMapNer != {} and isTrainFile:
-            print("Created NER label map from train file {}".format(readFile))
-            print(labelMapNer)
-            labelMapNerPath = os.path.join(wrtDir, "ner_{}_label_map.joblib".format(readFile.split('.')[0]))
-            joblib.dump(labelMapNer, labelMapNerPath)
-            print("label Map NER written at {}".format(labelMapNerPath))
+    print("NER File Written at {}".format(wrtDir))
+    print("POS File Written at {}".format(wrtDir))
+    #writing label map
+    if labelMapNer != {} and isTrainFile:
+        print("Created NER label map from train file {}".format(readFile))
+        print(labelMapNer)
+        labelMapNerPath = os.path.join(wrtDir, "ner_{}_label_map.joblib".format(readFile.split('.')[0]))
+        joblib.dump(labelMapNer, labelMapNerPath)
+        print("label Map NER written at {}".format(labelMapNerPath))
 
-        if labelMapPos != {} and isTrainFile:
-            print("Created POS label map from train file {}".format(readFile))
-            print(labelMapPos)
-            labelMapPosPath = os.path.join(wrtDir, "pos_{}_label_map.joblib".format(readFile.split('.')[0]))
-            joblib.dump(labelMapPos, labelMapPosPath)
-            print("label Map POS written at {}".format(labelMapPosPath))
+    if labelMapPos != {} and isTrainFile:
+        print("Created POS label map from train file {}".format(readFile))
+        print(labelMapPos)
+        labelMapPosPath = os.path.join(wrtDir, "pos_{}_label_map.joblib".format(readFile.split('.')[0]))
+        joblib.dump(labelMapPos, labelMapPosPath)
+        print("label Map POS written at {}".format(labelMapPosPath))
 
-        csv_file.close()
-        nerW.close()
-        posW.close()
+    csv_file.close()
+    nerW.close()
+    posW.close()
 
-        print('Max len of sentence: ', max(senLens))
-        print('Mean len of sentences: ', sum(senLens)/len(senLens))
-        print('Median len of sentences: ', median(senLens))
+    print('Max len of sentence: ', max(senLens))
+    print('Mean len of sentences: ', sum(senLens)/len(senLens))
+    print('Median len of sentences: ', median(senLens))
